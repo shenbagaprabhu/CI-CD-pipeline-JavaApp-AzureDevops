2938fffe64074e748c0c7ae8eb2fc8c46a7d3365
Shenbagaprabhu Palani Ganesan <62343058+shenbagaprabhu@users.noreply.github.com>
2024-12-06 14:45:29 +0530

    Update HelloController.java

--- app/src/main/java/hello/HelloController.java
+++ app/src/main/java/hello/HelloController.java
@@ -8,6 +8,6 @@ public class HelloController {
     
     @RequestMapping("/")
     public String index() {
-        return "Hello from Shenbagaprabhu";
+        return "Hello from Shenbagaprabhu Auto Trigger";
     }   
 }
